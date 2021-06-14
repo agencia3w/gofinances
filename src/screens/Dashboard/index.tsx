8be14175cde371a6170005e731a78041c37b4bf6
Paulinho <paulinho@agencia3w.com.br>
2021-06-14 08:15:34 -0300

    Listagem de transações do AsyncStorage

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import { useFocusEffect } from '@react-navigation/native';
+
 import { HighlightCard } from '../../components/HighlightCard';
 import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
 import {
@@ -23,41 +27,52 @@ export interface DataListProps extends TransactionCardProps {
 }
 
 export function Dashboard() {
-    const data: DataListProps[] = [
-        {
-            id: '1',
-            type: 'positive',
-            title: "Desenvolvimento de Site",
-            amount: "R$ 12.500,00",
-            category: {
-                name: 'Vendas',
-                icon: 'dollar-sign'
-            },
-            date: "13/04/2020"
-        },
-        {
-            id: '2',
-            type: 'negative',
-            title: "DiRango",
-            amount: "R$ 65,50",
-            category: {
-                name: 'Alimentação',
-                icon: 'coffee'
-            },
-            date: "13/04/2020"
-        },
-        {
-            id: '3',
-            type: 'negative',
-            title: "Aluguel",
-            amount: "R$ 900,00",
-            category: {
-                name: 'Casa',
-                icon: 'shopping-bag'
-            },
-            date: "13/04/2020"
-        },
-    ];
+    const [data, setData] = useState<DataListProps[]>([]);
+    const dataKey = '@gofinances:transactions';
+
+    async function loadTransactions() {
+        const response = await AsyncStorage.getItem(dataKey);
+        const transactions = response ? JSON.parse(response) : [];
+
+        const transactionsFormatted: DataListProps[] = transactions
+            .map((item: DataListProps) => {
+                const amount = Number(item.amount)
+                    .toLocaleString('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    });
+
+                const date = Intl.DateTimeFormat('pt-BR', {
+                    day: '2-digit',
+                    month: '2-digit',
+                    year: '2-digit'
+                }).format(new Date(item.date));
+
+                return {
+                    id: item.id,
+                    name: item.name,
+                    amount,
+                    type: item.type,
+                    category: item.category,
+                    date
+                }
+            });
+
+        setData(transactionsFormatted);
+    }
+
+    async function remove() {
+        await AsyncStorage.removeItem(dataKey);
+    }
+
+    useEffect(() => {
+        loadTransactions();
+        // remove();
+    }, []);
+
+    useFocusEffect(useCallback(() => {
+        loadTransactions();
+    },[]));
 
     return (
         <Container>
@@ -73,7 +88,7 @@ export function Dashboard() {
                         </User>
                     </UserInfo>
 
-                    <LogoutButton onPress={() => {}}>
+                    <LogoutButton onPress={() => { }}>
                         <Icon name="power" />
                     </LogoutButton>
                 </UserWrapper>
