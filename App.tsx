cc2ee868b4c0086511598d0c2ab6741806623265
Paulinho <paulinho@agencia3w.com.br>
2021-06-18 10:10:01 -0300

    Context API

--- App.tsx
+++ App.tsx
@@ -19,6 +19,7 @@ import {
 import theme from './src/global/styles/theme';
 import { AppRoutes } from './src/routes/app.routes';
 import { SignIn } from './src/screens/SignIn';
+import { AuthProvider } from './src/hooks/auth';
 
 export default function App() {
 	const [fontsLoaded] = useFonts({
@@ -35,7 +36,9 @@ export default function App() {
 		<ThemeProvider theme={theme}>
 			<NavigationContainer>
 				<StatusBar barStyle="light-content" />
-				<SignIn />
+				<AuthProvider>
+					<SignIn />
+				</AuthProvider>
 			</NavigationContainer>
 		</ThemeProvider>
 	)
