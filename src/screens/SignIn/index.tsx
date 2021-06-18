a9b4aa1009b80400bb048a1de34a0e73e9bd1614
Paulinho <paulinho@agencia3w.com.br>
2021-06-18 09:43:22 -0300

    Tela de Login

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import AppleSvg from '../../assets/apple.svg';
+import GoogleSvg from '../../assets/google.svg';
+import LogoSvg from '../../assets/logo.svg';
+
+import { SigInSocialButton } from '../../components/SigInSocialButton';
+
+import {
+    Container,
+    Header,
+    TitleWrapper,
+    Title,
+    SignInTitle,
+    Footer,
+    FooterWrapper
+} from './styles';
+
+export function SignIn() {
+    return (
+        <Container>
+            <Header>
+                <TitleWrapper>
+                    <LogoSvg
+                        width={RFValue(120)}
+                        height={RFValue(68)}
+                    />
+
+                    <Title>
+                        Controle suas{'\n'}
+                        finanças de forma{'\n'}
+                        muito simples
+                    </Title>
+
+                    <SignInTitle>
+                        Faça seu login{'\n'}
+                        com uma das contas abaixo
+                    </SignInTitle>
+                </TitleWrapper>
+
+            </Header>
+
+            <Footer>
+                <FooterWrapper>
+                    <SigInSocialButton
+                        title="Entrar com o Google"
+                        svg={GoogleSvg}
+                    />
+                    <SigInSocialButton
+                        title="Entrar com Apple"
+                        svg={AppleSvg}
+                    />
+                </FooterWrapper>
+            </Footer>
+        </Container>
+    )
+}
